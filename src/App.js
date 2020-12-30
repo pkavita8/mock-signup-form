e5967172c79deae9bae45952c81701d04cd29088
Kavita Patel <patel16kavita@gmail.com>
2020-12-30 09:54:58 +0530

    stack navigator created along with screens

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { StyleSheet, Text, View } from "react-native";
-const Stack = createStackNavigator();
-export default function App() {
-  return <NavigationContainer></NavigationContainer>;
-}
+import { Provider } from "react-redux";
+import store from "./store";
+import SignUp from "./screens/SignUp";
+import SetUpAccount from "./screens/SetUpAccount";
+import ConfirmDetails from "./screens/ConfirmDetails";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="SignUp">
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="SetUpAccount" component={SetUpAccount} />
+            <Stack.Screen name="ConfirmDetails" component={ConfirmDetails} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    );
+  }
+}
---
+++ src/screens/SetUpAccount.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SetUpAccount() {
+  return <div></div>;
+}
