7beacfa6f81bb7a42f19a4e7d32edbe3efe48848
Kavita Patel <patel16kavita@gmail.com>
2020-12-30 09:04:42 +0530

    setup for react navigation and app.js moved to src

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, Text, View } from "react-native";
+const Stack = createStackNavigator();
+export default function App() {
+  return <NavigationContainer></NavigationContainer>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
